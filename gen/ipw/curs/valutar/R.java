e4e4ff61d34d98b0311b384ccd6a6a3fdd14c6be
mariusc <marius@ipworkshop.ro>
2012-11-24 17:55:16 +0100

    updated icon and deleted useless files

--- gen/ipw/curs/valutar/R.java
+++ gen/ipw/curs/valutar/R.java
@@ -17,29 +17,24 @@ public final class R {
         public static final int tab_textcolor=0x7f070000;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int icon_archive_grey=0x7f020002;
-        public static final int icon_archive_white=0x7f020003;
-        public static final int icon_ss_grey=0x7f020004;
-        public static final int icon_ss_white=0x7f020005;
-        public static final int tab_bg_pressed=0x7f020006;
-        public static final int tab_bg_selected=0x7f020007;
-        public static final int tab_bg_unselected=0x7f020008;
-        public static final int tab_selector=0x7f020009;
+        public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int tab_bg_pressed=0x7f020002;
+        public static final int tab_bg_selected=0x7f020003;
+        public static final int tab_bg_unselected=0x7f020004;
+        public static final int tab_selector=0x7f020005;
     }
     public static final class id {
-        public static final int data=0x7f090006;
-        public static final int lista_dreapta=0x7f090005;
-        public static final int lista_stanga=0x7f090004;
-        public static final int menu_settings=0x7f09000a;
-        public static final int rezultat=0x7f090002;
-        public static final int spinner1=0x7f090001;
-        public static final int spinner2=0x7f090003;
-        public static final int sumaInitiala=0x7f090000;
-        public static final int tabTitleText=0x7f090007;
-        public static final int valoare=0x7f090009;
-        public static final int valuta=0x7f090008;
+        public static final int data=0x7f080006;
+        public static final int lista_dreapta=0x7f080005;
+        public static final int lista_stanga=0x7f080004;
+        public static final int rezultat=0x7f080002;
+        public static final int spinner1=0x7f080001;
+        public static final int spinner2=0x7f080003;
+        public static final int sumaInitiala=0x7f080000;
+        public static final int tabTitleText=0x7f080007;
+        public static final int valoare=0x7f080009;
+        public static final int valuta=0x7f080008;
     }
     public static final class layout {
         public static final int convertor=0x7f030000;
@@ -47,9 +42,6 @@ public final class R {
         public static final int custom_tab=0x7f030002;
         public static final int list_item=0x7f030003;
         public static final int main=0x7f030004;
-    }
-    public static final class menu {
-        public static final int activity_main=0x7f080000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
